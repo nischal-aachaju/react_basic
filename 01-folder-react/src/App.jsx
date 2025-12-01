b8b1a150aaf1a579366c2125ef33f33821191efc
Nischal Shrestha <sthanischal5060@gmail.com>
2025-12-01 11:28:11 +0545

    react app all files

---
+++ 01-folder-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ 
+const App = () => {
+  return (
+   <>
+    <div id='parent1'>
+      <h1 id='child1'>Hello parent1</h1>
+      <h2 id='child2'>Nischal stha</h2>
+    </div>
+    <div id='parent2'>
+      <h1 id='child1'>Hello parent2</h1>
+      <h2 id='child2'>Nischal shrestha</h2>
+    </div>
+   </>
+  )
+}
+
+export default App
