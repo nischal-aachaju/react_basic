58818f9e38ee46636a59a37d7402a9c1a4b50516
Nischal Shrestha <sthanischal5060@gmail.com>
2025-12-01 11:33:28 +0545

    1st commit

--- 01-folder-react/src/App.jsx
+++ 01-folder-react/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
    <>
     <div id='parent1'>
       <h1 id='child1'>Hello parent1</h1>
-      <h2 id='child2'>Nischal stha</h2>
+      <p id='child2'>Nischal Shrestha</p>
     </div>
     <div id='parent2'>
       <h1 id='child1'>Hello parent2</h1>
